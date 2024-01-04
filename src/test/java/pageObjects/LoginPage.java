a48ea5e90a013a146e9507a60d0284d11c93f379
emilianocastillo <emiliano.castillo@testpro.io>
2024-01-04 13:32:42 -0400

    BasePage setup also added a simple login test to make sure test are executing.

--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -1,4 +1,44 @@
 package pageObjects;
 
-public class LoginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+
+public class LoginPage extends BasePage {
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    ///////////////////SELECTORS\\\\\\\\\\\\\\\\\\\\\
+
+    private final By emailInput = By.cssSelector("input[type='email']");
+    private final By passwordInput = By.cssSelector("input[type='password']");
+    private final By loginBtn = By.cssSelector("button[type='submit']");
+
+    //////////////////METHODS\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    public LoginPage enterEmail (String email) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(emailInput)).sendKeys(email);
+        return this;
+    }
+
+    public LoginPage enterPassword (String password) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(passwordInput)).sendKeys(password);
+        return this;
+    }
+
+    public LoginPage clickLoginBtn () {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(loginBtn)).click();
+        return this;
+    }
+
+    /////////////////ASSERTIONS\\\\\\\\\\\\\\\\\\\\\\
+
+    public void verifyLoginSuccessful() {
+        WebElement avatar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img.avatar")));
+        Assert.assertTrue(avatar.isDisplayed());
+    }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,5 +1,20 @@
 package tests;
 
-public class LoginTests extends BaseTest{
+import org.testng.annotations.Test;
+import pageObjects.LoginPage;
+
+public class LoginTests extends BaseTest {
+
+    LoginPage loginPage;
+
+    @Test
+    public void loginWithValidCreds(){
+        loginPage = new LoginPage(driver);
+
+        loginPage.enterEmail("emiliano.castillo@testpro.io")
+                 .enterPassword("Et!@&eV!6K&bk6jLg@EhqQW")
+                 .clickLoginBtn()
+                 .verifyLoginSuccessful();
+    }
 
 }
