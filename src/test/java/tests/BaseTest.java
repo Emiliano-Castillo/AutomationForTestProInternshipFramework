064fc2e3603fb12dd64926b3d54cd1f95fa3e7ca
emilianocastillo <emiliano.castillo@testpro.io>
2024-01-04 13:01:18 -0400

    BaseTest setup

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,4 +1,36 @@
 package tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import java.time.Duration;
+
 public class BaseTest {
+    public WebDriver driver;
+
+    protected String url;
+
+    @BeforeMethod
+    @Parameters({"BaseURL"})
+    public void browserSetUp(String BaseURL){
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().deleteAllCookies();
+        url = BaseURL;
+        driver.get(BaseURL);
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
+    }
 }
